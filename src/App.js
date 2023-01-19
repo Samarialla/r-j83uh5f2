53fac33d4ea2d581ca10785f2db1ce3450383d6f
Sergio Amarilla <sergojavier23@gmail.com>
2023-01-18 21:51:14 -0300

    init

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      data: null,
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea onChange={(e)=>this.setState({data:e.target.value})} rows="3">{this.state.data}</textarea>
+        <div className="counter">{this.state.data ? this.state.data.length: '0'}</div>
       </div>
     );
   }
